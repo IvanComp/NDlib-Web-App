e7ba97efb0b02300de43b09ed87a10042d46e11d
Ivan <ivan.compagnucci@studenti.unicam.it>
2024-05-31 15:34:03 +0200

    Project Initialization

---
+++ src/main/java/ndlib/views/simulate/SimulateView.java
@@ -0,0 +1,35 @@
+package ndlib.views.simulate;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import ndlib.views.MainLayout;
+
+@PageTitle("Simulate")
+@Route(value = "", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class SimulateView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public SimulateView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
